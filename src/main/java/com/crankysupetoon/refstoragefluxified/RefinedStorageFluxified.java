deaee03246f730a99687c4a6254fd4f50c50c517
CrankySupertoon <crankysupertoon@gmail.com>
2020-05-15 15:33:14 -0400

    1.0.0

---
+++ src/main/java/com/crankysupetoon/refstoragefluxified/RefinedStorageFluxified.java
@@ -0,0 +1,9 @@
+package com.crankysupetoon.enderiofluxified;
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(modid = RefinedStorageFluxified.MODID, version = RefinedStorageFluxified.VERSION)
+public class RefinedStorageFluxified {
+	public static final String MODID = "refstoragefluxified";
+	public static final String VERSION = "1.0.0";
+	}
